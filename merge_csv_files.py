5626f6f50f8eb8776a197713db010908bd48fff6
Ji ZHANG <hope.zh@gmail.com>
2020-06-04 12:48:25 +0800

    create folder to save merged csv if it's not there

--- merge_csv_files.py
+++ merge_csv_files.py
@@ -1,6 +1,5 @@
 """
 merge multiple csv files in one directory
-
 """
 
 # --------------------------------------------------------
@@ -10,21 +9,29 @@ import shutil
 import pandas as pd
 from datetime import datetime
 
+
 # --------------------------------------------------------
 # get start time stamp
 start_time = datetime.now()
 start_time_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
 print('start date & time:\n', start_time_str, '\n')
 
-# --------------------------------------------------------
-def concat_csv_files(path_csv, path_and_name_merged_csv, cwd):
-    if os.path.exists(path_and_name_merged_csv):
-        try:
-            os.remove(path_and_name_merged_csv)
-        except OSError:
-            print("ERROR!")
 
-    os.chdir(path_csv)
+# --------------------------------------------------------
+def concat_csv_files(_dir_csv, _dir_merged_csv, _path_merged_csv, _cwd):
+    if os.path.exists(_dir_merged_csv):
+        if os.path.exists(_path_merged_csv):
+            try:
+                os.remove(_path_merged_csv)
+            except OSError:
+                print("ERROR!")
+    else: 
+        try:
+            os.mkdir(_dir_merged_csv)
+        except OSError:
+            print('unable to create directory!')
+
+    os.chdir(_dir_csv)
     csvList = glob.glob('*.csv')
     # print('csv file list:\n', csvList)
 
@@ -45,36 +52,44 @@ def concat_csv_files(path_csv, path_and_name_merged_csv, cwd):
         print(df.head(), '\n')
 
     concatDf = pd.concat(dfList, axis=0)
-    concatDf.to_csv(path_and_name_merged_csv, index=None)
+    concatDf.to_csv(_path_merged_csv, index=None)
 
-    print('output file is:\n', path_and_name_merged_csv, '\n')
+    print('merged csv file is:\n', _path_merged_csv, '\n')
     print('shape:\n', concatDf.shape, '\n')
-    print('description:\n', concatDf.describe(), '\n')
     print('head:\n', concatDf.head(), '\n')
+    print('tail:\n', concatDf.tail(), '\n')
+    print('description:\n', concatDf.describe(), '\n')
+    print('info:\n', concatDf.info(), '\n')
 
     os.chdir(cwd)
+
 
 # --------------------------------------------------------
 # get the cwd of this script
 cwd = os.getcwd()
 print('full path of current working directory:', '\n', cwd, '\n')
 
+
 # --------------------------------------------------------
 # specify the directory of the csv files
 dir_csv  = 'data'
 path_csv = os.path.join(cwd, dir_csv)
-print('full path of csv files:\n', path_csv, '\n')
+print('path of csv files:\n', path_csv, '\n')
+
 
 # --------------------------------------------------------
 # specify the path and name of merged csv file
-dir_merged_csv  = 'merged_csv'
-merged_csv_file_name = 'merged_results.csv'
-path_and_name_merged_csv = os.path.join(cwd, dir_merged_csv, merged_csv_file_name)
-print('full path and name of merged csv:\n', path_and_name_merged_csv, '\n')
+merged_csv_dir_name  = 'merged_csv'
+merged_csv_name = 'merged_results.csv'
+dir_merged_csv = os.path.join(cwd, merged_csv_dir_name)
+path_merged_csv = os.path.join(cwd, dir_merged_csv, merged_csv_name)
+print('full path of merged csv:\n', path_merged_csv, '\n')
+
 
 # --------------------------------------------------------
 # concate csv files
-concat_csv_files(path_csv, path_and_name_merged_csv, cwd)
+concat_csv_files(path_csv, dir_merged_csv, path_merged_csv, cwd)
+
 
 # --------------------------------------------------------
 # print finish time stamp and time used
